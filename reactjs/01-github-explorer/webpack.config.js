dec5bf9d4bb97d41b2bfa7715cc4b02d77695f85
Gilson da Gama <gilsondagama@hotmail.com>
2022-03-01 11:18:58 -0300

    Estrutura do ReactJS

--- reactjs/01-github-explorer/webpack.config.js
+++ reactjs/01-github-explorer/webpack.config.js
@@ -1,10 +1,11 @@
 const path = require('path')
 
 module.exports = {   
+    mode: 'development',
     entry: path.resolve(__dirname, 'src', 'index.jsx'),
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js'
+        filename: 'build.js',
     },
     resolve: {
         extensions: ['.js', '.jsx'],
