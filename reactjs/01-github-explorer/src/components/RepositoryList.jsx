df53930ee4b1b34f414f078806c83133ceee540e
Gilson da Gama <gilsondagama@hotmail.com>
2022-03-02 12:00:22 -0300

    Primeiro component React

---
+++ reactjs/01-github-explorer/src/components/RepositoryList.jsx
@@ -0,0 +1,38 @@
+export function RepositoryList() {
+  return (
+    <section className="repository-list">
+      <h1>Lista de Repositórios</h1>
+
+      <ul>
+        <li>
+          <strong>unform</strong>
+          <p>Forms in react</p>
+
+          <a href="">
+            Acessar repositório
+          </a>
+        </li>
+        
+        <li>
+          <strong>unform</strong>
+          <p>Forms in react</p>
+
+          <a href="">
+            Acessar repositório
+          </a>
+        </li>
+        
+        <li>
+          <strong>unform</strong>
+          <p>Forms in react</p>
+
+          <a href="">
+            Acessar repositório
+          </a>
+        </li>
+
+      </ul>
+
+    </section>
+  )
+}
