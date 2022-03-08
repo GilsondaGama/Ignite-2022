f2e9cd2ff5a34d480c0e11c9dccd6a22a4756180
Gilson da Gama <gilsondagama@hotmail.com>
2022-03-08 08:56:51 -0300

    Component: transaction Table

---
+++ reactjs/02-dtmoney/src/components/TransactionsTable/index.tsx
@@ -0,0 +1,32 @@
+import { Container } from "./styles";
+
+export function TransactionTable() {
+  return(
+    <Container>
+      <table>
+        <thead>
+          <tr>
+            <th>Títuto</th>
+            <th>Valor</th>
+            <th>Categoria</th>
+            <th>Data</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Desenvolvimento de website</td>
+            <td className="deposit">R$12.000</td>
+            <td>Desenvolvimento</td>
+            <td>22/01/2022</td>
+          </tr>
+          <tr>
+            <td>Aluguel</td>
+            <td className="withdraw">- R$3.000</td>
+            <td>©asa</td>
+            <td>11/04/2022</td>
+          </tr>
+        </tbody>
+      </table>
+    </Container>
+  )
+}
